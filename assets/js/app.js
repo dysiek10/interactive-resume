d0b36252cf6d6fcb8ab1fa52c0a3466a167956a4
Edyta Wiśniewska <eedyta.wisniewska@gmail.com>
2022-01-04 13:39:58 +0100

    Add navigation and hamburger scratch

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+let hamburger = document.querySelector('.hamburger');
+let contact = document.querySelector('.contact');
+let data = document.querySelector('.data');
+
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('is-active');
+    contact.classList.toggle('menu-open');
+    data.classList.toggle('menu-open');
+})
