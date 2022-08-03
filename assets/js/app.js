a0067ef2ac122ac36647b5d4cf2549acfea74201
Edyta Wiśniewska <eedyta.wisniewska@gmail.com>
2022-08-03 16:22:48 +0200

    Initial commit - project with JS practice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-let hamburger = document.querySelector('.hamburger');
-let contact = document.querySelector('.contact');
-let data = document.querySelector('.data');
+let hamburger = document.querySelector(".hamburger");
+let contact = document.querySelector(".contact");
+let data = document.querySelector(".data");
 
-
-hamburger.addEventListener('click', function () {
-    hamburger.classList.toggle('is-active');
-    contact.classList.toggle('menu-open');
-    data.classList.toggle('menu-open');
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  contact.classList.toggle("menu-open");
+  data.classList.toggle("menu-open");
+});
