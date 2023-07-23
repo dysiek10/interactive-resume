6fe4ec1246a1f3ad3f7ed6e05b928b2c0034c342
Edyta Wiśniewska <eedyta.wisniewska@gmail.com>
2023-07-23 16:54:06 +0200

    Update experience, skills and projects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
-let hamburger = document.querySelector(".hamburger");
-let contact = document.querySelector(".contact");
-let data = document.querySelector(".data");
+// Hambuger
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("is-active");
-  contact.classList.toggle("menu-open");
-  data.classList.toggle("menu-open");
-});
+// let hamburger = document.querySelector(".hamburger");
+// let contact = document.querySelector(".contact");
+// let data = document.querySelector(".data");
+
+// hamburger.addEventListener("click", function () {
+//   hamburger.classList.toggle("is-active");
+//   contact.classList.toggle("menu-open");
+//   data.classList.toggle("menu-open");
+// });
+
+// See more - experience
+let oldExperienceEls = document.querySelectorAll(".experience .item.old");
+console.log(oldExperienceEls);
+let seeMoreBtn = document.querySelector("see-more");
